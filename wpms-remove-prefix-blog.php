be0ce4c82fbbc359b03d267a198598babfa1e1bd
herewithme <amaury@beapi.fr>
2012-01-04 09:31:52 +0100

    remove old readme, add plugin code

---
+++ wpms-remove-prefix-blog.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ Plugin Name: WPMS - Remove prefix /blog/
+ Version: 1.0
+ Plugin URI: http://www.beapi.fr
+ Description: Remove prefix /blog/ for main website of WordPress Network Installation.
+ Author: BeAPI
+ Author URI: http://www.beapi.fr
+
+ Copyright 2011 - BeAPI Team (technique@beapi.fr)
+ */
+
+// Check permalink update,category base update and tag base update
+add_filter('pre_update_option_' . 'category_base', 'wpms_remove_prefix_blog');
+add_filter('pre_update_option_' . 'tag_base', 'wpms_remove_prefix_blog');
+add_filter('pre_update_option_' . 'permalink_structure', 'wpms_remove_prefix_blog');
+
+/**
+ * Just check if the current structure begins with /blog/ remove that and return the stripped structure
+ */
+function wpms_remove_prefix_blog($structure_permalink) {
+	if ( substr($structure_permalink, 0, 6) != '/blog/' )
+		return $structure_permalink;
+	
+	return substr($structure_permalink, 6, strlen($structure_permalink));
+}
+?>
